631f0ff28543c51aa8574ca9ec50c20a52e2d3dd
ofurlan <33640072+ofurlan@users.noreply.github.com>
2022-08-24 11:43:20 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,3 @@
 node default {
-  file {'/root/README':
-      ensure  => file,
-      content => 'hola mundo',
-      owner   => 'root',
-   }
-   file {'/root/README':
-      owner   => 'root',
-   }
+
 }
