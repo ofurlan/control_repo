dc4ee177da70df7ac9b9c14d5f29f852c3f59449
ofurlan <33640072+ofurlan@users.noreply.github.com>
2022-08-24 15:28:23 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 node default {
-   include role::db_server
+   
 }
-#node 'ubgcp8tst1.c.hardies-prod-host.internal' {
-#   include role::master_server
-#}
+node 'ubgcp8tst1.c.hardies-prod-host.internal' {
+   include role::master_server
+}
